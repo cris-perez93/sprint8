9e8fca8f27a54193c6ccb6abb9aba5cdbb019928
Cris <cristian.h.perez.o@gmail.com>
2021-04-26 20:30:43 +0100

    subida

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-export default () => {
+import React, {useEffect, useState} from 'react';
+import axios from "axios"
 
-    return (
-        <div>
-            hello World!
-        </div>
-    );
-};
+
+function App() {
+
+    const [acudit, setAcudit] = useState('')
+
+   
+     const consultaApi = async () => {
+            const result = await axios ('https://icanhazdadjoke.com/', {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+
+            setAcudit(result.data.joke)
+
+            
+
+        }
+        
+        
+return (
+     <div className="container">
+     <h1>Preparat per riure?</h1>
+     <div className="acudit">
+         <p>{acudit}</p>
+     </div>
+     <input
+       type="submit"
+       value ="Siguiente"
+       onClick ={consultaApi}
+       
+    />
+     </div>
+ );
+}
+  
+  export default App;
